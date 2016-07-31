a4b015a71134a7ce1b059ae004073470eef8405f
lang25150 <shuaili_2013@163.com>
2016-07-31 21:34:43 +0800

    test提交测试

---
+++ src/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+
+public class test {
+	
+	public static void print(String str){
+			System.out.println(str);
+		}
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print("Hello World!!");
+	}
+
+}
